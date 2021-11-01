f567103ad4830f57386dd51b268c0f79398b0c0b
Melissa Ots <meots@tktk.ee>
2021-11-01 21:44:18 +0200

    add new projects

---
+++ FirstOrLast/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FirstOrLast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada ees- ja perekonnanime
+            //programm kontrollib andmete pikkust (eraldi)
+            //programm kuvab kumb neist on pikem, kas ees- või perekonnanimi
+
+            Console.WriteLine("Sisesta eesnimi:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Sisesta perekonnanimi:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine($"Sinu eesnimes on {firstName.Length} sümbolit ja perekonnanimes {lastName.Length} sümbolit.");
+
+            if (firstName.Length > lastName.Length)
+            {
+                Console.WriteLine("Sinu eesnimes on rohkem sümboleid.");
+            }
+            else if (firstName.Length < lastName.Length)
+            {
+                Console.WriteLine("Sinu perekonnanimes on rohkem sümboleid.");
+            }
+            else
+            {
+                Console.WriteLine("Ees- ja perekonnanimes on võrdselt sümboleid!");
+            }
+        }
+    }
+}
---
+++ HOLCounter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HOLCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab konsoolis, mitu 'h', 'o' ja 'l' tähte on lauses "Hello World!"
+
+            string HelloWorld = ("hello world!");
+
+            int hCounter = 0;
+            int oCounter = 0;
+            int lCounter = 0;
+
+            for (int i = 0; i < HelloWorld.Length; i++)
+            {
+                if (HelloWorld[i] == 'h')
+                {
+                    hCounter++;
+                }
+                if (HelloWorld[i] == 'o')
+                {
+                    oCounter++;
+                }
+                if (HelloWorld[i] == 'l')
+                {
+                    lCounter++;
+                }
+            }
+
+            Console.WriteLine($"Lauses 'Hello World!' on {hCounter} 'h', {oCounter} 'o' ja {lCounter} 'l' tähte. ");
+        }
+    }
+}
